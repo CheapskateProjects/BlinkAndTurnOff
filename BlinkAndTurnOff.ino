70be83ed5ff635f0872e18514d7f19a553e4b2d6
Cheapskateprojects <cheapskateprojects@gmail.com>
2018-03-17 22:26:15 +0200

    Fixed description

--- BlinkAndTurnOff.ino
+++ BlinkAndTurnOff.ino
@@ -2,7 +2,7 @@
   Blink and turn off
 
   This is a test program for low power Adruino which only powers on on hardware level when required (e.g. vibration sensor is triggered). 
-  The Arduino flashes led for a few times and then turns itself off by setting pin 2 to low (which will trigger transistor to power it off). 
+  The Arduino flashes led for a few times and then turns itself off by setting pin 2 to high (which will trigger transistor to power it off). 
 
   created   Mar 2018
   by CheapskateProjects
